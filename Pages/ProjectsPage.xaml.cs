bbc2b16a4b819ec7828ebb57cb626aa486e84fc3
Mugsend <chaudharysaurabh93063@gmail.com>
2025-02-26 17:36:33 +0530

    added files

--- Pages/ProjectsPage.xaml.cs
+++ Pages/ProjectsPage.xaml.cs
@@ -7,6 +7,7 @@ using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using WinUIPortfolio.Models;
 using System.Diagnostics.CodeAnalysis;
+using DotEnv.Core;
 namespace WinUIPortfolio.Pages
 {
     public sealed partial class ProjectsPage : Page
@@ -26,8 +27,9 @@ namespace WinUIPortfolio.Pages
                 LoadingIndicator.IsActive = true;
                 LoadingIndicator.Visibility = Visibility.Visible;
                 var apiUrl = "https://api.github.com/user/repos";
+                new EnvLoader().Load();
 
-                var personalAccessToken = "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";
+                var personalAccessToken = Environment.GetEnvironmentVariable("API_TOKEN");
 
                 httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("WinUIPortfolioApp", "1.0"));
                 httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("token", personalAccessToken);
@@ -39,14 +41,12 @@ namespace WinUIPortfolio.Pages
                 var repositories = JsonSerializer.Deserialize<List<GitHubRepository>>(jsonResponse);
                 
 
-                // Bind the data to the UI (assuming you have a ListView named ReposListView)
                 ReposListView.ItemsSource = repositories;
                 LoadingIndicator.IsActive = false;
                 LoadingIndicator.Visibility = Visibility.Collapsed;
             }
             catch (Exception ex)
             {
-                // Handle exceptions
                 Console.WriteLine($"Error fetching repositories: {ex.Message}");
             }
         }
